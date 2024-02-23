a45f1b519f1056877be01f48c1a53ee98a0d68d7
sevgiyilmaz <sevgiyilmaz2112@gmail.com>
2024-02-24 00:40:59 +0300

    add notes

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -17,11 +17,30 @@ namespace Arrays
 
             string[] students2 = new string[3]{"A", "B", "C" };
             string[] students3 = { "A", "B", "C" };
-            students3[3] = "D";
+            // students3[3] = "D"; // error
 
             foreach (string s in students)
             {
                 Console.WriteLine(s);
+            }
+
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++) // 0. olan 5 kısmı
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++) // 1. olan 3 kısmı
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("********");
+
             }
 
             Console.WriteLine();
